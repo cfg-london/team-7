a5116fb028a50c1cf6ceb9f3cc78c1a6d13e4545
louisehaggett <lrh215@exeter.ac.uk>
2017-11-17 21:47:41 +0000

    dropdown using php to get the indicators

---
+++ dropdown.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<select name="indicator">
+<?php
+require_once "";
+$query = "SELECT indicator
+FROM database ORDER BY name ASC";
+error_log ($query);
+$result = $db ->query($query);
+while ($row = $result ->fetch_assoc()) {
+echo ’<option value="’
+echo $row["indicator"] . ’">’;
+echo $row["indicator"]
+echo ’</option >’;
+}
+? >
+</select >

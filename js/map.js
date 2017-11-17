f32c9dc8663e760d2fa0d8e535a8a693389a2bbc
Jordan Tucker <Jordan@Jordans-MacBook-Pro.local>
2017-11-17 21:52:52 +0000

    Indicator drop down

--- js/map.js
+++ js/map.js
@@ -1,12 +1,52 @@
-$(window).resize(function(){
-
-});
-
-var basic = new Datamap({
+var map = new Datamap({
   element: document.getElementById("mapWrapper"),
   projection: 'mercator',
   responsive: true,
   fills: {
-    defaultFill: "#333333"
-  }
+    defaultFill: "rgb(85,186,178)",
+    includedCountry: "rgb(112,3,112)"
+  },
+  data : {}
 });
+
+function addCountry(country){
+  var a = country;
+  var obj = {};
+  obj[a] = {fillKey : "includedCountry"};
+  console.log(obj);
+  map.updateChoropleth(obj);
+}
+
+function sizeChange() {
+    d3.select("g").attr("transform", "scale(" + $("#mapWrapper").width()/1680 + ")");
+    $("svg").height($("#container").height());
+}
+
+d3.select(window).on("resize", sizeChange);
+
+$(document).ready(function(){
+  sizeChange();
+  addCountry("KEN");
+  addCountry("IDN");
+  addCountry("IND");
+  addCountry("ESA");
+  addCountry("SEN");
+  addCountry("COL");
+  addCountry("AUS");
+});
+
+/*var myGeoJSONPath = 'json/world.json';
+var myCustomStyle = {
+    stroke: false,
+    fill: true,
+    fillColor: '#fff',
+    fillOpacity: 1
+}
+$.getJSON(myGeoJSONPath,function(data){
+    var map = L.map('map').setView([39.74739, -105], 4);
+
+    L.geoJson(data, {
+        clickable: false,
+        style: myCustomStyle
+    }).addTo(map);
+})*/

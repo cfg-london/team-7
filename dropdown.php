e5f7b626622a1c29e12c46c6455660aa8b435d18
Jordan Tucker <Jordan@Jordans-MacBook-Pro.local>
2017-11-17 22:12:05 +0000

    new map

--- dropdown.php
+++ dropdown.php
@@ -10,3 +10,15 @@ echo $row["indicator"] . ’">’;
 echo $row["indicator"]
 echo ’</option >’;
 }
+require_once "";
+$query = "SELECT ages
+FROM database ORDER BY name ASC";
+error_log ($query);
+$result = $db ->query($query);
+while ($row = $result ->fetch_assoc()) {
+echo ’<option value="’
+echo $row["ages"] . ’">’;
+echo $row["ages"]
+echo ’</option >’;
+}
+?>
